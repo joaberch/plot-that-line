1e21ab34407dcef9c7713d6f3615bc7be9dc96c3
joaberch <122774888+joaberch@users.noreply.github.com>
2024-09-13 14:40:10 +0200

    feat(form2): Add a title

--- code/plot-that-lines/plot-that-lines/Form2.Designer.cs
+++ code/plot-that-lines/plot-that-lines/Form2.Designer.cs
@@ -28,27 +28,39 @@ namespace plot_that_lines
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent(string title)
+        private void InitializeComponent(string headerTitle)
         {
             //Form2
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = title;
+            this.Text = headerTitle;
 
             //ScottPlott
             SuspendLayout();
             FormsPlot formsPlot = new FormsPlot
             {
-                //DisplayScale = 10000,
+                DisplayScale = 10000,
                 Location = new Point(100, 50),
-                Name = title,
+                Name = headerTitle,
                 Size = new Size(600, 300),
                 TabIndex = 0,
             };
             ResumeLayout(false);
 
+            //title
+            Label title = new Label();
+            title.AutoSize = true;
+            title.Font = new Font("Segoe UI", 24F);
+            title.Location = new Point(275, 5);
+            title.Name = "Titre";
+            title.Size = new Size(215, 45);
+            title.TabIndex = 2;
+            title.Text = headerTitle;
+
+            //add to forms
             Controls.Add(formsPlot);
+            Controls.Add(title);
         }
 
         #endregion
