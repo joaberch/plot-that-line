2a76076dc26e67a2251aae97719b49e516821c94
joaberch <122774888+joaberch@users.noreply.github.com>
2024-09-27 15:34:31 +0200

    feat(API): connect to an API

--- code/plot-that-lines/plot-that-lines/Program.cs
+++ code/plot-that-lines/plot-that-lines/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic.ApplicationServices;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace plot_that_lines
 {
@@ -12,9 +14,49 @@ namespace plot_that_lines
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            
+
+            _ = GetUserById();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+        }
+        /// <summary>
+        /// This asynchronous method sends a GET request to
+        /// a REST API endpoint and retrieves a user by id.
+        static async Task<User?> GetUserById()
+        {
+            string apiKey = "0";
+            StreamReader sr2 = new StreamReader(".env");
+            string line2;
+
+            while ((line2 = sr2.ReadLine()) != null)
+            {
+                if (line2.Contains("APIKEY")) {
+                    apiKey = line2.Split(" = ").Last();
+                }
+            }
+
+            try
+            {
+                using var client = new HttpClient();
+                var url = $"https://api.getgeoapi.com/v2/currency/convert?api_key={apiKey}";
+
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<User>(responseContent);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
         }
     }
 }
