b22852d27d18125c1b28cdbcc56457f77028d5fc
joaberch <122774888+joaberch@users.noreply.github.com>
2024-10-11 16:32:26 +0200

    feat(convert): convert money in another devise

--- code/plot-that-lines/plot-that-lines/Program.cs
+++ code/plot-that-lines/plot-that-lines/Program.cs
@@ -58,5 +58,42 @@ namespace plot_that_lines
                 return null;
             }
         }
+
+        static async Task<User?> Convert(string inputCurrency, string convertToCurrency, int amount)
+        {
+            string apiKey = "0";
+            StreamReader sr2 = new StreamReader(".env");
+            string line2;
+
+            while ((line2 = sr2.ReadLine()) != null)
+            {
+                if (line2.Contains("APIKEY"))
+                {
+                    apiKey = line2.Split(" = ").Last();
+                }
+            }
+
+            try
+            {
+                using var client = new HttpClient();
+                var url = $"https://api.getgeoapi.com/v2/currency/convert?api_key={apiKey}&from={inputCurrency}&to={convertToCurrency}&amount={amount}&format=json";
+
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return JsonSerializer.Deserialize<User>(responseContent);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+        }
     }
 }
