8358cf00c0c9052702d3bc07f3e9e4529dac00bf
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-10-24 12:36:32 +0200

    chore(form2 & program): remove useless line of code

--- code/plot-that-lines/plot-that-lines/Program.cs
+++ code/plot-that-lines/plot-that-lines/Program.cs
@@ -15,85 +15,8 @@ namespace plot_that_lines
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
 
-            _ = GetUserById();
-
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-        }
-        /// <summary>
-        /// This asynchronous method sends a GET request to
-        /// a REST API endpoint and retrieves a user by id.
-        static async Task<User?> GetUserById()
-        {
-            string apiKey = "0";
-            StreamReader sr2 = new StreamReader(".env");
-            string line2;
-
-            while ((line2 = sr2.ReadLine()) != null)
-            {
-                if (line2.Contains("APIKEY")) {
-                    apiKey = line2.Split(" = ").Last();
-                }
-            }
-
-            try
-            {
-                using var client = new HttpClient();
-                var url = $"https://api.getgeoapi.com/v2/currency/convert?api_key={apiKey}";
-
-                var response = await client.GetAsync(url);
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    return JsonSerializer.Deserialize<User>(responseContent);
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
-        }
-
-        static async Task<User?> Convert(string inputCurrency, string convertToCurrency, int amount)
-        {
-            string apiKey = "0";
-            StreamReader sr2 = new StreamReader(".env");
-            string line2;
-
-            while ((line2 = sr2.ReadLine()) != null)
-            {
-                if (line2.Contains("APIKEY"))
-                {
-                    apiKey = line2.Split(" = ").Last();
-                }
-            }
-
-            try
-            {
-                using var client = new HttpClient();
-                var url = $"https://api.getgeoapi.com/v2/currency/convert?api_key={apiKey}&from={inputCurrency}&to={convertToCurrency}&amount={amount}&format=json";
-
-                var response = await client.GetAsync(url);
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseContent = await response.Content.ReadAsStringAsync();
-                    return JsonSerializer.Deserialize<User>(responseContent);
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return null;
-            }
         }
     }
 }
